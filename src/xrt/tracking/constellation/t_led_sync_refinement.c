639f30d2db6bc5301fb6239c2fadd247d4c1122b
Beyley Cardellio <ep1cm1n10n123@gmail.com>
2026-05-09 19:11:24 -0400

    t/constellation: Add basic LED sync implementation
    Signed-off-by: Beyley Cardellio <ep1cm1n10n123@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2852>

---
+++ src/xrt/tracking/constellation/t_led_sync_refinement.c
@@ -0,0 +1,505 @@
+// Copyright 2026, Beyley Cardellio
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  A routine to automatically refine latency offsets of LED blink times using constellation samples.
+ * @author Beyley Cardellio <ep1cm1n10n123@gmail.com>
+ * @ingroup tracking
+ */
+
+#include "util/u_var.h"
+#include "util/u_debug.h"
+
+#include "math/m_api.h"
+
+#include "os/os_time.h"
+
+#include "t_led_sync_refinement.h"
+
+
+DEBUG_GET_ONCE_LOG_OPTION(led_sync_refinement_log, "T_LED_SYNC_REFINEMENT_LOG", U_LOGGING_INFO)
+
+#define LOG_TRACE(refinement, ...) U_LOG_IFL_T(refinement->log_level, __VA_ARGS__)
+#define LOG_DEBUG(refinement, ...) U_LOG_IFL_D(refinement->log_level, __VA_ARGS__)
+#define LOG_INFO(refinement, ...) U_LOG_IFL_I(refinement->log_level, __VA_ARGS__)
+#define LOG_WARN(refinement, ...) U_LOG_IFL_W(refinement->log_level, __VA_ARGS__)
+#define LOG_ERROR(refinement, ...) U_LOG_IFL_E(refinement->log_level, __VA_ARGS__)
+
+//! The amount of frames to not see it for consider a device "not found" during an active search
+#define MAX_SAMPLE_LATENCY 3
+
+/*
+ * Helper functions
+ */
+
+//! The amount of time of which a change under this amount is considered "good" and we can exit the binary search
+static time_duration_ns
+change_good_cutoff(struct t_led_sync_refinement *refinement)
+{
+	return MIN(refinement->current_blink_duration_ns, refinement->exposure_time_ns) / 20;
+}
+
+//! The amount of frames since the sample was applied to the controller
+static uint32_t
+frames_since_sample_apply(struct t_led_sync_refinement *refinement, timepoint_ns now_ns)
+{
+	return (uint32_t)((now_ns - refinement->last_sample_apply_time_ns) / refinement->exposure_interval_ns);
+}
+
+static void
+set_offset_duration(struct t_led_sync_refinement *refinement,
+                    time_duration_ns fudge_offset_ns,
+                    time_duration_ns blink_duration_ns)
+{
+	refinement->current_offset_ns = fudge_offset_ns;
+	refinement->current_blink_duration_ns = blink_duration_ns;
+
+	refinement->has_sample_for_driver = true;
+	refinement->sample_applied = false;
+	refinement->sample_for_driver = XRT_C11_COMPOUND(struct t_led_sync_sample){
+	    .fudge_offset_ns = fudge_offset_ns,
+	    .blink_duration_ns = blink_duration_ns,
+	};
+
+	LOG_TRACE(refinement, "Setting offset to %" PRId64 "ns and blink duration to %" PRId64 "ns", fudge_offset_ns,
+	          blink_duration_ns);
+}
+
+//! Gets the period to iterate over when searching for a latency offset.
+static time_duration_ns
+get_search_period_locked(struct t_led_sync_refinement *refinement)
+{
+	int divisor = 4;
+
+	if (!refinement->has_exposure_time) {
+		return refinement->current_blink_duration_ns / divisor;
+	}
+
+	if (refinement->phase == T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET) {
+		// If we're in initial offset search, we can use the larger of the two, since we just need to find one
+		// that overlaps.
+		return MAX(refinement->current_blink_duration_ns, refinement->exposure_time_ns) / divisor;
+	} else {
+		return MIN(refinement->current_blink_duration_ns, refinement->exposure_time_ns) / divisor;
+	}
+}
+
+static void
+set_phase_locked(struct t_led_sync_refinement *refinement, enum t_led_sync_phase new_phase)
+{
+	refinement->phase = new_phase;
+
+	LOG_INFO(refinement, "Entering search phase: %d", new_phase);
+
+	switch (new_phase) {
+	case T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET: {
+		refinement->found_left_edge_ns = -1;
+		refinement->found_right_edge_ns = -1;
+
+		set_offset_duration(refinement, 0, refinement->current_blink_duration_ns);
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_FIND_RIGHT_EDGE: {
+		// Left bound is the current offset
+		refinement->binary_search_state.left_bound_ns = refinement->current_offset_ns;
+		// Right bound is the start of the next exposure minus the blink duration, we don't want to blink into
+		// the next frame, rather start at zero, since latency will always be >0.
+		refinement->binary_search_state.right_bound_ns =
+		    refinement->exposure_interval_ns - refinement->current_blink_duration_ns;
+
+		LOG_DEBUG(refinement,
+		          "Starting binary search for right edge, left bound at: %" PRId64
+		          "ns, right bound at: %" PRId64 "ns",
+		          refinement->binary_search_state.left_bound_ns,
+		          refinement->binary_search_state.right_bound_ns);
+
+		time_duration_ns new_offset_ns =
+		    (refinement->binary_search_state.right_bound_ns + refinement->binary_search_state.left_bound_ns) /
+		    2;
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_FIND_LEFT_EDGE: {
+		// We should always have a right edge by now
+		assert(refinement->found_right_edge_ns > -1);
+
+		// Left bound is when the exposure *should* be given zero latency
+		refinement->binary_search_state.left_bound_ns = 0;
+		// Right bound is the found right edge
+		refinement->binary_search_state.right_bound_ns = refinement->found_right_edge_ns;
+
+		LOG_DEBUG(refinement, "Starting binary search for left edge, right edge at: %" PRId64 "ns",
+		          refinement->found_right_edge_ns);
+
+		time_duration_ns new_offset_ns =
+		    (refinement->binary_search_state.right_bound_ns + refinement->binary_search_state.left_bound_ns) /
+		    2;
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_MAINTAIN_OFFSET: {
+		// We should have both edges by now
+		assert(refinement->found_right_edge_ns > -1);
+		assert(refinement->found_left_edge_ns > -1);
+
+		// Set the offset so that the center of the blink is at the center of the exposure
+		time_duration_ns new_offset_ns =
+		    ((refinement->found_left_edge_ns + refinement->found_right_edge_ns) / 2) -
+		    (refinement->current_blink_duration_ns / 2);
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+
+		LOG_DEBUG(refinement, "Maintaining offset at: %" PRId64 "ns", refinement->current_offset_ns);
+
+		break;
+	}
+	default: break;
+	}
+}
+
+static void
+handle_find_initial_offset(struct t_led_sync_refinement *refinement)
+{
+	if (refinement->frames_since_last_visually_seen > MAX_SAMPLE_LATENCY) {
+		const time_duration_ns search_period_ns = get_search_period_locked(refinement);
+		const time_duration_ns new_offset_ns =
+		    (refinement->current_offset_ns + search_period_ns) % refinement->exposure_interval_ns;
+
+		LOG_DEBUG(refinement, "Device hasn't been seen, trying offset: %" PRId64 "ns", new_offset_ns);
+
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+	} else {
+		LOG_DEBUG(refinement, "Device has been seen, moving to find edge phases");
+
+		// We got a sample recently, try to find the edges now.
+		set_phase_locked(refinement, T_LED_SYNC_SEARCH_PHASE_FIND_RIGHT_EDGE);
+	}
+}
+
+static void
+handle_find_right_edge(struct t_led_sync_refinement *refinement)
+{
+	if (refinement->frames_since_last_visually_seen > MAX_SAMPLE_LATENCY) {
+		LOG_DEBUG(refinement,
+		          "Device hasn't been seen for %" PRIu32
+		          " frames, advancing binary search with left side closer",
+		          refinement->frames_since_last_visually_seen);
+
+		// We didn't see the device
+		refinement->binary_search_state.right_bound_ns = refinement->current_offset_ns;
+
+		time_duration_ns new_offset_ns =
+		    (refinement->binary_search_state.left_bound_ns + refinement->binary_search_state.right_bound_ns) /
+		    2;
+
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+
+	} else {
+		LOG_DEBUG(refinement, "Device has been seen, advancing binary search with right side closer");
+
+		// We saw the device
+		refinement->binary_search_state.left_bound_ns = refinement->current_offset_ns;
+
+		time_duration_ns new_offset_ns =
+		    (refinement->binary_search_state.left_bound_ns + refinement->binary_search_state.right_bound_ns) /
+		    2;
+
+		set_offset_duration(refinement, new_offset_ns, refinement->current_blink_duration_ns);
+	}
+
+	bool done = refinement->binary_search_state.right_bound_ns - refinement->binary_search_state.left_bound_ns <
+	            change_good_cutoff(refinement);
+	if (done) {
+		// We found a good enough offset for the right edge, move on to finding the left edge.
+		refinement->found_right_edge_ns = refinement->current_offset_ns;
+
+		LOG_DEBUG(refinement,
+		          "Found right edge at offset: %" PRId64 "ns. Difference %" PRId64
+		          "ns was lower than cutoff %" PRId64 "ns, moving to find left edge phase",
+		          refinement->current_offset_ns,
+		          refinement->binary_search_state.right_bound_ns -
+		              refinement->binary_search_state.left_bound_ns,
+		          change_good_cutoff(refinement));
+
+		set_phase_locked(refinement, T_LED_SYNC_SEARCH_PHASE_FIND_LEFT_EDGE);
+	}
+}
+
+static void
+handle_find_left_edge(struct t_led_sync_refinement *refinement)
+{
+	if (refinement->frames_since_last_visually_seen > MAX_SAMPLE_LATENCY) {
+		LOG_DEBUG(refinement,
+		          "Device hasn't been seen for %" PRIu32
+		          " frames, advancing binary search with right side closer",
+		          refinement->frames_since_last_visually_seen);
+
+		// We didn't see the device
+		refinement->binary_search_state.left_bound_ns =
+		    refinement->current_offset_ns + refinement->current_blink_duration_ns;
+
+		time_duration_ns new_offset_ns =
+		    ((refinement->binary_search_state.left_bound_ns + refinement->binary_search_state.right_bound_ns) /
+		     2);
+		// Set the new offset so that we're moving the right edge of the blink to the left edge of the exposure
+		set_offset_duration(refinement, new_offset_ns - refinement->current_blink_duration_ns,
+		                    refinement->current_blink_duration_ns);
+	} else {
+		LOG_DEBUG(refinement, "Device has been seen, advancing binary search with left side closer");
+
+		// We saw the device
+		refinement->binary_search_state.right_bound_ns =
+		    refinement->current_offset_ns + refinement->current_blink_duration_ns;
+
+		time_duration_ns new_offset_ns =
+		    (refinement->binary_search_state.left_bound_ns + refinement->binary_search_state.right_bound_ns) /
+		    2;
+		// Set the new offset so that we're moving the right edge of the blink to the left edge of the exposure
+		set_offset_duration(refinement, new_offset_ns - refinement->current_blink_duration_ns,
+		                    refinement->current_blink_duration_ns);
+	}
+
+	bool done = refinement->binary_search_state.right_bound_ns - refinement->binary_search_state.left_bound_ns <
+	            change_good_cutoff(refinement);
+	if (done) {
+		// We found a good enough offset for the left edge, we can maintain the offset now.
+		refinement->found_left_edge_ns = refinement->current_offset_ns + refinement->current_blink_duration_ns;
+
+		LOG_DEBUG(refinement,
+		          "Found left edge at offset: %" PRId64 "ns. Difference %" PRId64
+		          "ns was lower than cutoff %" PRId64 "ns, moving to maintain offset phase",
+		          refinement->current_offset_ns,
+		          refinement->binary_search_state.right_bound_ns -
+		              refinement->binary_search_state.left_bound_ns,
+		          change_good_cutoff(refinement));
+
+		set_phase_locked(refinement, T_LED_SYNC_SEARCH_PHASE_MAINTAIN_OFFSET);
+	}
+}
+
+static void
+handle_maintain_offset(struct t_led_sync_refinement *refinement)
+{}
+
+static void
+handle_timing_event_locked(struct t_led_sync_refinement *refinement,
+                           const struct t_timing_event_camera_exposure_start *event)
+{
+	if (event->exposure_time_ns > 0) {
+		refinement->has_exposure_time = true;
+		refinement->exposure_time_ns = event->exposure_time_ns;
+	}
+
+	assert(event->frame_period_ns > 0); // No frame period present. Please estimate this externally.
+
+	refinement->exposure_interval_ns = event->frame_period_ns;
+	refinement->frames_since_last_visually_seen++;
+	refinement->current_sequence_id = event->sequence_id;
+
+	// We have an exposure interval now, start searching!
+	if (refinement->phase == T_LED_SYNC_SEARCH_PHASE_INIT) {
+		set_phase_locked(refinement, T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET);
+	}
+
+	// Not applied yet
+	if (!refinement->sample_applied) {
+		return;
+	}
+
+	timepoint_ns now_ns = os_monotonic_get_ns();
+
+	// Let the device settle for a few frames after applying a sample before we start trying to refine again
+	if (frames_since_sample_apply(refinement, now_ns) < refinement->options.settle_frames) {
+		return;
+	}
+
+	bool trigger_resync = refinement->frames_since_last_visually_seen >
+	                          (refinement->options.time_to_resync_ns / refinement->exposure_interval_ns) &&
+	                      refinement->phase > T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET;
+	// If we're past the initial find and we haven't seen the controller in some amount of
+	// time, start searching for an offset again.
+	if (trigger_resync) {
+		LOG_DEBUG(refinement, "Triggering resync due to not being visually seen for %" PRIu32 " frames",
+		          refinement->frames_since_last_visually_seen);
+		set_phase_locked(refinement, T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET);
+	}
+
+	switch (refinement->phase) {
+	case T_LED_SYNC_SEARCH_PHASE_INIT: return;
+	case T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET: {
+		handle_find_initial_offset(refinement);
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_FIND_RIGHT_EDGE: {
+		handle_find_right_edge(refinement);
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_FIND_LEFT_EDGE: {
+		handle_find_left_edge(refinement);
+		break;
+	}
+	case T_LED_SYNC_SEARCH_PHASE_MAINTAIN_OFFSET: {
+		handle_maintain_offset(refinement);
+		break;
+	}
+	default: break;
+	}
+}
+
+static void
+handle_constellation_sample_locked(struct t_led_sync_refinement *refinement,
+                                   const struct t_constellation_tracker_sample *sample)
+{
+	refinement->frames_since_last_visually_seen = 0;
+}
+
+/*
+ * Exported functions
+ */
+
+int
+t_led_sync_refinement_init(struct t_led_sync_refinement *refinement,
+                           const struct t_led_sync_refinement_options *options)
+{
+	int result;
+
+	(*refinement) = XRT_C11_COMPOUND(struct t_led_sync_refinement){
+	    .initialized = false,
+
+	    .log_level = debug_get_log_option_led_sync_refinement_log(),
+
+	    .options = *options,
+
+	    .has_exposure_time = false,
+	    .exposure_time_ns = 0,
+
+	    .exposure_interval_ns = 0,
+
+	    // Push an initial sample
+	    .has_sample_for_driver = true,
+	    .sample_for_driver =
+	        {
+	            .fudge_offset_ns = refinement->current_offset_ns,
+	            .blink_duration_ns = refinement->current_blink_duration_ns,
+	        },
+	    .sample_applied = false,
+
+	    .phase = T_LED_SYNC_SEARCH_PHASE_INIT,
+	    .current_offset_ns = 0,
+	    .current_blink_duration_ns = options->initial_blink_duration_ns,
+	    .frames_since_last_visually_seen = 0,
+	    .current_sequence_id = 0,
+	    .last_sample_apply_time_ns = 0,
+
+	    .found_left_edge_ns = -1,
+	    .found_right_edge_ns = -1,
+
+	    .binary_search_state =
+	        {
+	            .left_bound_ns = 0,
+	            .right_bound_ns = 0,
+	        },
+	};
+
+	result = os_mutex_init(&refinement->lock);
+	if (result < 0) {
+		return result;
+	}
+
+	refinement->initialized = true;
+
+	u_var_add_root(refinement, "LED Sync Refinement", true);
+
+	u_var_add_gui_header(refinement, NULL, "Options");
+	u_var_add_ro_i32(refinement, (int32_t *)&refinement->options.flags, "flags");
+	u_var_add_ro_i64_ns(refinement, &refinement->options.initial_blink_duration_ns, "initial_blink_duration_ns");
+
+	u_var_add_gui_header(refinement, NULL, "Exposure Info");
+	u_var_add_bool(refinement, &refinement->has_exposure_time, "Has Exposure Time");
+	u_var_add_ro_i64_ns(refinement, &refinement->exposure_time_ns, "Exposure Time (ns)");
+	u_var_add_ro_i64_ns(refinement, &refinement->exposure_interval_ns, "Exposure Interval (ns)");
+
+	u_var_add_gui_header(refinement, NULL, "Driver Sample");
+	u_var_add_bool(refinement, &refinement->has_sample_for_driver, "Has Sample For Driver");
+	u_var_add_ro_i64_ns(refinement, &refinement->sample_for_driver.fudge_offset_ns, "Sample Fudge Offset (ns)");
+	u_var_add_ro_i64_ns(refinement, &refinement->sample_for_driver.blink_duration_ns, "Sample Blink Duration (ns)");
+
+	u_var_add_gui_header(refinement, NULL, "State");
+	u_var_add_ro_u32(refinement, (uint32_t *)&refinement->phase, "phase");
+	u_var_add_ro_i64_ns(refinement, &refinement->current_offset_ns, "current_offset_ns");
+	u_var_add_ro_i64_ns(refinement, &refinement->current_blink_duration_ns, "current_blink_duration_ns");
+	u_var_add_ro_u32(refinement, &refinement->frames_since_last_visually_seen, "frames_since_last_visually_seen");
+	u_var_add_ro_u32(refinement, &refinement->current_sequence_id, "current_sequence_id");
+	u_var_add_ro_i64_ns(refinement, &refinement->last_sample_apply_time_ns, "last_sample_apply_time_ns");
+	u_var_add_ro_i64_ns(refinement, &refinement->found_left_edge_ns, "found_left_edge_ns");
+	u_var_add_ro_i64_ns(refinement, &refinement->found_right_edge_ns, "found_right_edge_ns");
+	u_var_add_ro_i64_ns(refinement, &refinement->binary_search_state.left_bound_ns, "binary_search.left_bound_ns");
+	u_var_add_ro_i64_ns(refinement, &refinement->binary_search_state.right_bound_ns,
+	                    "binary_search.right_bound_ns");
+
+	return 0;
+}
+
+void
+t_led_sync_refinement_destroy(struct t_led_sync_refinement *refinement)
+{
+	if (!refinement->initialized) {
+		return;
+	}
+
+	os_mutex_destroy(&refinement->lock);
+}
+
+void
+t_led_sync_push_timing_event(struct t_led_sync_refinement *refinement,
+                             const struct t_timing_event_camera_exposure_start *event)
+{
+	assert(refinement->initialized);
+
+	os_mutex_lock(&refinement->lock);
+	handle_timing_event_locked(refinement, event);
+	os_mutex_unlock(&refinement->lock);
+}
+
+void
+t_led_sync_push_constellation_sample(struct t_led_sync_refinement *refinement,
+                                     const struct t_constellation_tracker_sample *sample)
+{
+	assert(refinement->initialized);
+
+	os_mutex_lock(&refinement->lock);
+	handle_constellation_sample_locked(refinement, sample);
+	os_mutex_unlock(&refinement->lock);
+}
+
+bool
+t_led_sync_get_sample(struct t_led_sync_refinement *refinement, struct t_led_sync_sample *out_sample)
+{
+	assert(refinement->initialized);
+
+	os_mutex_lock(&refinement->lock);
+	{
+		if (!refinement->has_sample_for_driver) {
+			os_mutex_unlock(&refinement->lock);
+			return false;
+		}
+
+		*out_sample = refinement->sample_for_driver;
+		refinement->has_sample_for_driver = false;
+	}
+	os_mutex_unlock(&refinement->lock);
+
+	return true;
+}
+
+void
+t_led_sync_mark_latest_sample_applied(struct t_led_sync_refinement *refinement, timepoint_ns apply_time_ns)
+{
+	assert(refinement->initialized);
+
+	os_mutex_lock(&refinement->lock);
+	refinement->sample_applied = true;
+	refinement->last_sample_apply_time_ns = apply_time_ns;
+	os_mutex_unlock(&refinement->lock);
+}
---
+++ src/xrt/tracking/constellation/t_led_sync_refinement.h
@@ -0,0 +1,154 @@
+// Copyright 2026, Beyley Cardellio
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  A routine to automatically refine latency offsets of LED blink times using constellation samples.
+ * @author Beyley Cardellio <ep1cm1n10n123@gmail.com>
+ * @ingroup tracking
+ */
+
+#pragma once
+
+#include "util/u_time.h"
+#include "util/u_logging.h"
+
+#include "os/os_threading.h"
+
+#include "tracking/t_time_sync.h"
+#include "tracking/t_constellation.h"
+
+
+//! 10 seconds after last visually seen sample to resync
+#define T_LED_SYNC_DEFAULT_RESYNC_TIME (time_duration_ns)(U_TIME_1S_IN_NS * 5LL)
+
+//! Flags to control the behavior of the LED sync refinement routine.
+enum t_led_sync_refinement_flags
+{
+	T_LED_SYNC_REFINEMENT_FLAGS_NONE = 0,
+};
+
+//! Options for the LED sync refinement.
+struct t_led_sync_refinement_options
+{
+	//! The flags to use when refining.
+	enum t_led_sync_refinement_flags flags;
+
+	//! How long the LEDs should be initially blinking for, in nanoseconds.
+	time_duration_ns initial_blink_duration_ns;
+
+	//! The minimum duration to use for the LED blinks, if using reduction.
+	time_duration_ns min_blink_duration_ns;
+	//! The maximum duration to use for the LED blinks, if using reduction.
+	time_duration_ns max_blink_duration_ns;
+
+	//! Time to wait after the last visually seen sample before resyncing.
+	time_duration_ns time_to_resync_ns;
+	//! Frames to wait after the sample was applied to let the device settle.
+	uint32_t settle_frames;
+};
+
+//! A sample read out from the driver, to pass to the device in question.
+struct t_led_sync_sample
+{
+	//! The latency offset to add to the camera exposure time.
+	time_duration_ns fudge_offset_ns;
+	//! The duration to make the LEDs blink for, in nanoseconds.
+	time_duration_ns blink_duration_ns;
+};
+
+enum t_led_sync_phase
+{
+	//! The initial phase, where we haven't made any adjustments yet.
+	T_LED_SYNC_SEARCH_PHASE_INIT = 0,
+	//! Trying to find some offset that gets us in sync at all.
+	T_LED_SYNC_SEARCH_PHASE_FIND_INITIAL_OFFSET = 1,
+	//! Trying to align the rising edge of the LED blinks with the falling edge of the camera exposure.
+	T_LED_SYNC_SEARCH_PHASE_FIND_RIGHT_EDGE = 2,
+	//! Trying to align the falling edge of the LED blinks with the rising edge of the camera exposure.
+	T_LED_SYNC_SEARCH_PHASE_FIND_LEFT_EDGE = 3,
+	//! We've found an offset and optimized the center of the LED blink to the center of the exposure.
+	T_LED_SYNC_SEARCH_PHASE_MAINTAIN_OFFSET = 4,
+};
+
+struct t_led_sync_refinement
+{
+	//! Whether the structure has been fully initialized.
+	bool initialized;
+
+	enum u_logging_level log_level;
+
+	//! The options to use for refinement
+	struct t_led_sync_refinement_options options;
+
+	struct os_mutex lock;
+
+	/*!
+	 * Whether we know the actual exposure time of the frames. We may not, so we have to work without it in some
+	 * cases.
+	 */
+	bool has_exposure_time;
+	//! The known time the frame was exposed for.
+	time_duration_ns exposure_time_ns;
+
+	//! The known interval between frames.
+	time_duration_ns exposure_interval_ns;
+
+	//! Whether we have a sample ready to be sent to the driver/device.
+	bool has_sample_for_driver;
+	//! The sample to be sent to the driver/device on it's next convenience.
+	struct t_led_sync_sample sample_for_driver;
+	//! Whether the sample has been applied
+	bool sample_applied;
+
+	//! The current search phase
+	enum t_led_sync_phase phase;
+
+	//! The current estimated latency offset between the camera exposure and the LED blink.
+	time_duration_ns current_offset_ns;
+
+	//! How long the LEDs blink for, each frame.
+	time_duration_ns current_blink_duration_ns;
+
+	//! The amount of frames since the controller was last visually seen.
+	uint32_t frames_since_last_visually_seen;
+
+	//! The sequence ID of the latest timing event we processed.
+	uint32_t current_sequence_id;
+
+	//! The time the latest sample was applied to the driver/device
+	timepoint_ns last_sample_apply_time_ns;
+
+	//! The found left edge of the exposure, -1 if not found yet.
+	time_duration_ns found_left_edge_ns;
+	//! The found right edge of the exposure, -1 if not found yet.
+	time_duration_ns found_right_edge_ns;
+
+	struct
+	{
+		//! The current left bound of the binary search, if we're in a find edge phase.
+		time_duration_ns left_bound_ns;
+		//! The current right bound of the binary search, if we're in a find edge phase.
+		time_duration_ns right_bound_ns;
+	} binary_search_state;
+};
+
+int
+t_led_sync_refinement_init(struct t_led_sync_refinement *refinement,
+                           const struct t_led_sync_refinement_options *options);
+
+void
+t_led_sync_refinement_destroy(struct t_led_sync_refinement *refinement);
+
+void
+t_led_sync_push_timing_event(struct t_led_sync_refinement *refinement,
+                             const struct t_timing_event_camera_exposure_start *event);
+
+void
+t_led_sync_push_constellation_sample(struct t_led_sync_refinement *refinement,
+                                     const struct t_constellation_tracker_sample *sample);
+
+bool
+t_led_sync_get_sample(struct t_led_sync_refinement *refinement, struct t_led_sync_sample *out_sample);
+
+void
+t_led_sync_mark_latest_sample_applied(struct t_led_sync_refinement *refinement, timepoint_ns apply_time_ns);
